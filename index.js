3df4357792a10d952272c873bbfae3f8b28d2b05
AdamTHM <34792961+AdamTHM@users.noreply.github.com>
2020-10-24 02:21:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
 const client = new Discord.Client();
+const fs = require('fs');
 
 client.once('ready', () => {
 	console.log('da bot is now the loaded');
