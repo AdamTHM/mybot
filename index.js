a96c942b4cbf9f82c45a513fcad010213a7d12b7
AdamTHM <34792961+AdamTHM@users.noreply.github.com>
2020-10-24 02:26:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const fs = require('fs');
 
 client.commands = new Discord.Collection();
 
-client.once('ready', () => {
-	console.log('da bot is now the loaded');
-});
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 
 client.login(config.token);
